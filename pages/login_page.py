0f70bb9669424e81ca84fa2741f8efc9815ec6c8
Firstname Lastname <daritoss@MacBook-Pro-Daria.local>
2026-08-20 23:14:48 +0300

    IlCarro_Login_NEG_HW

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,11 @@
 import time
 
+from selenium.common import TimeoutException
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
 
 def pause(seconds):
     time.sleep(seconds)
@@ -10,28 +15,83 @@ class LoginPage:
 
     LOGIN_NAV = (By.CSS_SELECTOR, "[href='/login']")
     EMAIL_INPUT = (By.XPATH, "//input[@name='username']")
-    PASSWORD_INPUT = (By.CSS_SELECTOR, "[name='password']")
+    PASSWORD_INPUT = (By.CSS_SELECTOR, "input[name='password']")
     YALLA_BTN = (By.XPATH, "//button[contains(text(),'Y’alla!')]")
+    CONFIRMATION_TEXT = (By.CSS_SELECTOR, "h3")
+    OK_BTN = (By.XPATH, "//*[text()='OK']")
+    LOG_OUT_BTN = (By.XPATH, "//*[text()='Log out']")
+    ERROR_EMPTY = (By.XPATH, "//*[contains(text(), 'is required')]")
+    WRONG_EMAIL = (By.XPATH, "//*[text()='Wrong email format']")
+    UNSUCCESS_LOGIN_TEXT = (By.CSS_SELECTOR, "p")
 
     def __init__(self, driver):
         self.driver = driver
 
     def open_login_form(self):
         self.driver.find_element(*self.LOGIN_NAV).click()
-        pause(1)
+        pause(2)
 
 
     def fill_email(self, email):
         self.driver.find_element(*self.EMAIL_INPUT).clear()
-        pause(1)
         self.driver.find_element(*self.EMAIL_INPUT).send_keys(email)
-        pause(1)
 
     def fill_password(self, password):
         self.driver.find_element(*self.PASSWORD_INPUT).clear()
-        pause(1)
         self.driver.find_element(*self.PASSWORD_INPUT).send_keys(password)
-        pause(1)
 
     def submit_login(self):
         self.driver.find_element(*self.YALLA_BTN).click()
+
+    def login(self, email, password): #это просто краткая версия подтверждения логина
+        self.fill_email(email)
+        self.fill_password(password)
+        self.submit_login()
+
+    def login_success_text(self):
+        # return self.driver.find_element(*self.CONFIRMATION_TEXT).text
+        element = WebDriverWait(self.driver, timeout=5).until(
+            expected_conditions.visibility_of_element_located(self.CONFIRMATION_TEXT)
+        )
+        return element.text
+
+
+
+    def close_window(self):
+        self.driver.find_element(*self.OK_BTN).click()
+
+    def is_logged(self):
+        try:
+            WebDriverWait(self.driver, timeout=5).until(
+                expected_conditions.visibility_of_element_located(self.LOG_OUT_BTN)
+                # можно еще написать как EC.visibility_of_element_located
+                # только в случае, когда сверху мы поменяла expected conditions на ec
+            )
+            return True
+        except TimeoutException :
+            return False
+
+
+
+    def error_is_empty(self):
+        element = WebDriverWait(self.driver, timeout=5).until(
+            expected_conditions.visibility_of_element_located(self.ERROR_EMPTY)
+        )
+        return element.text
+
+
+    def wrong_email_format(self):
+        element = WebDriverWait(self.driver, timeout=5).until(
+            expected_conditions.visibility_of_element_located(self.WRONG_EMAIL)
+        )
+        return element.text
+
+    def login_unsuccess_text(self):
+        element = WebDriverWait(self.driver, timeout=5).until(
+            expected_conditions.visibility_of_element_located(self.UNSUCCESS_LOGIN_TEXT)
+        )
+        return element.text
+
+    def error_for_password(self):
+        self.driver.find_element(*self.PASSWORD_INPUT).click()
+
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,12 +1,111 @@
+import time
+
 from pages.login_page import LoginPage
 
 VALID_EMAIL = 'dar.kaikova@gmail.com'
 VALID_PASSWORD = 'Dar123456!'
+INVALID_EMAIL = 'dar.kaikovagmail.com'
+INVALID_EMAIL_2 = 'darIA.kaikova@gmail.com'
+INVALID_PASSWORD = 'Dar123456!12'
+UNREGISTERED_USER_EMAIL = 'darion.kaikova@gmail.com'
+UNREGISTERED_USER_PASSWORD = 'sakadam_badimbadum12!'
+
+
 
 def test_login_success(driver):
     login_page = LoginPage(driver)
 
     login_page.open_login_form()
-    login_page.fill_email(VALID_EMAIL)
+    login_page.fill_email(VALID_EMAIL) #не проходит, так как почта не успевает встать для проверки (слишком быстро)(поставила паузы для помощи)
     login_page.fill_password(VALID_PASSWORD)
     login_page.submit_login()
+
+    assert login_page.login_success_text() == "You are logged in success"
+    login_page.close_window()
+
+    assert login_page.is_logged() is True
+
+
+
+def test_login_success_1(driver):
+    login_page = LoginPage(driver)
+
+    login_page.open_login_form()
+    login_page.login(VALID_EMAIL, VALID_PASSWORD)
+
+    assert login_page.login_success_text() == "You are logged in success"
+    login_page.close_window()
+
+    assert login_page.is_logged() is True
+
+
+def test_with_empty_email(driver):
+    login_page = LoginPage(driver)
+
+    login_page.open_login_form()
+    login_page.fill_email('')
+    login_page.fill_password(VALID_PASSWORD)
+
+    assert login_page.error_is_empty() == "Email is required"
+
+
+def test_with_empty_password(driver):
+    login_page = LoginPage(driver)
+
+    login_page.open_login_form()
+    login_page.error_for_password()
+    login_page.fill_email(VALID_EMAIL)
+
+    assert login_page.error_is_empty() == "Password is required"
+
+
+def test_wrong_email_format(driver):
+    login_page = LoginPage(driver)
+
+    login_page.open_login_form()
+    login_page.fill_email(INVALID_EMAIL)
+    login_page.fill_password(VALID_PASSWORD)
+
+    assert login_page.wrong_email_format() == 'Wrong email format'
+
+def test_valid_email_invalid_password(driver):
+    login_page = LoginPage(driver)
+
+    login_page.open_login_form()
+    login_page.fill_email(VALID_EMAIL)
+    login_page.fill_password(INVALID_PASSWORD)
+    login_page.submit_login()
+
+    assert login_page.login_unsuccess_text() == '"Login or Password incorrect"'
+    login_page.close_window()
+
+
+def test_invalid_email_valid_password(driver):
+    login_page = LoginPage(driver)
+
+    login_page.open_login_form()
+    login_page.fill_email(INVALID_EMAIL_2)
+    login_page.fill_password(VALID_PASSWORD)
+    login_page.submit_login()
+
+    assert login_page.login_unsuccess_text() == '"Login or Password incorrect"'
+    login_page.close_window()
+
+
+
+def test_unregistered_user(driver):
+    login_page = LoginPage(driver)
+
+    login_page.open_login_form()
+    login_page.fill_email(UNREGISTERED_USER_EMAIL)
+    login_page.fill_password(UNREGISTERED_USER_PASSWORD)
+    login_page.submit_login()
+
+    assert login_page.login_unsuccess_text() == '"Login or Password incorrect"'
+    login_page.close_window()
+
+
+
+
+
+
